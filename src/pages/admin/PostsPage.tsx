a9fa045b6f3a272387b52de426c0902c2af2e702
m3taroid <rodriguengoy3@gmail.com>
2026-01-22 02:54:40 +0100

    fix

--- src/pages/admin/PostsPage.tsx
+++ src/pages/admin/PostsPage.tsx
@@ -81,7 +81,7 @@ const PostsPage: React.FC = () => {
     } | null>(null);
 
     const {data: categories} = UseGetCategories({noPermission: 1, type: "news"})
-    const {data: posts, isPending: isGettingPosts, isRefetching: isReGettingPosts refetch: reGetPosts} = UseGetPosts({format: "stats"})
+    const {data: posts, isPending: isGettingPosts, isRefetching: isReGettingPosts, refetch: reGetPosts} = UseGetPosts({format: "stats"})
     const {data: addResult, isPending: isAdding, mutate: addPost} = UseAddPost()
     const {data: updateResult, isPending: isUpdating, mutate: updatePost} = UseUpdatePost()
     const {data: deleteResult, isPending: isDeleting, mutate: deletePost} = UseDeletePost()
